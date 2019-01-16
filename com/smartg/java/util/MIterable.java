caab9fc5c649f172266b80528f54352fd86fb8d6
Andrey Kuznetsov <andronix@gmx.net>
2019-01-16 22:43:14 +0100

    Create MIterable.java

---
+++ com/smartg/java/util/MIterable.java
@@ -0,0 +1,67 @@
+package com.smartg.java.util;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * Iterator items from multiple Iterators 
+ * @author User
+ *
+ * @param <E>
+ */
+public class MIterable<E> implements Iterable<E> {
+
+	private final List<Iterator<E>> list = new ArrayList<>();
+
+	public void add(Iterable<E> iterable) {
+		list.add(iterable.iterator());
+	}
+
+	public void add(Iterator<E> iterator) {
+		list.add(iterator);
+	}
+
+	static class MIterator<E> implements Iterator<E> {
+		private final List<Iterator<E>> list;
+		private Iterator<Iterator<E>> iterator;
+		private Iterator<E> current;
+
+		public MIterator(List<Iterator<E>> list) {
+			this.list = list;
+		}
+
+		@Override
+		public boolean hasNext() {
+			Iterator<E> iter = getIterator();
+			return iter != null && iter.hasNext();
+		}
+
+		private Iterator<E> getIterator() {
+			if (current != null && current.hasNext()) {
+				return current;
+			}
+			if (iterator == null) {
+				iterator = list.iterator();
+			}
+			if (iterator.hasNext()) {
+				current = iterator.next();
+			}
+			return current;
+		}
+
+		@Override
+		public E next() {
+			Iterator<E> iter = getIterator();
+			if (iter == null) {
+				return null;
+			}
+			return iter.next();
+		}
+	}
+
+	@Override
+	public Iterator<E> iterator() {
+		return new MIterator<>(list);
+	}
+}
