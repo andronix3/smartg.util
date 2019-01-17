b71456758b0da2b695a12dc46947c4a06dd99c72
Andrey Kuznetsov <andronix@gmx.net>
2019-01-17 08:49:54 +0100

    Create MappingIterator.java

---
+++ com/smartg/java/util/MappingIterator.java
@@ -0,0 +1,33 @@
+package com.smartg.java.util;
+
+import java.util.Iterator;
+import java.util.function.Function;
+
+/**
+ * Iterate and apply Function "on the fly"
+ * @author User
+ *
+ * @param <T>
+ * @param <E>
+ */
+public class MappingIterator<T, E> implements Iterator<E> {
+
+	private Function<T, E> mapper;
+	private Iterator<T> iterator;
+
+	public MappingIterator(Function<T, E> mapper, Iterator<T> iterator) {
+		this.mapper = mapper;
+		this.iterator = iterator;
+	}
+
+	@Override
+	public boolean hasNext() {
+		return iterator.hasNext();
+	}
+
+	@Override
+	public E next() {
+		return mapper.apply(iterator.next());
+	}
+
+}
